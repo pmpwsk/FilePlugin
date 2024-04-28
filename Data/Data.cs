fb2ce94b40d98fe3ae5c5c96e2cedb2350f95ebb
pmpwsk <flo@uwap.org>
2024-04-28 13:30:59 +0200

    Added CheckAccess.

--- Data/Data.cs
+++ Data/Data.cs
@@ -17,4 +17,73 @@ public partial class FilePlugin : Plugin
         Table[key] = profile;
         return profile;
     }
+
+    private bool CheckAccess(IRequest req, string userId, string[] segments, bool wantsEdit, out Profile? profile, out DirectoryNode? parent, out DirectoryNode? directory, out FileNode? file, out string name)
+    {
+        string? accessKey = req.LoggedIn ? req.User.Id : null;
+        bool hasAccess = accessKey == userId;
+        parent = null; directory = null; file = null; name = "?";
+        if (segments[0] != "")
+        {
+            profile = null;
+            return false;
+        }
+        if (!Table.TryGetValue($"{req.UserTable.Name}_{userId}", out profile))
+            return false;
+        DirectoryNode? current = profile.RootNode;
+        CheckAccess(current);
+        if (segments.Length == 1)
+        {
+            if (hasAccess)
+            {
+                directory = current;
+                if (req.UserTable.TryGetValue(userId, out var user))
+                    name = '@' + user.Username;
+            }
+            return true;
+        }
+        foreach (string segment in segments.Skip(1).SkipLast(1))
+            if (current.Directories.TryGetValue(segment, out current))
+                CheckAccess(current);
+            else return false;
+        name = segments.Last();
+        if (current.Directories.TryGetValue(name, out var d))
+        {
+            if (hasAccess)
+            {
+                if (current != d)
+                    parent = current;
+                directory = d;
+            }
+            else
+            {
+                CheckAccess(d);
+                if (hasAccess)
+                    directory = d;
+            }
+            return true;
+        }
+        else if (current.Files.TryGetValue(name, out var f))
+        {
+            if (hasAccess)
+            {
+                parent = current;
+                file = f;
+            }
+            return true;
+        }
+        else return false;
+
+        void CheckAccess(DirectoryNode dir)
+        {
+            if (hasAccess)
+                return;
+            var access = dir.ShareAccess;
+            if ((accessKey != null && CheckAccess(accessKey)) || CheckAccess("*"))
+                hasAccess = true;
+
+            bool CheckAccess(string k)
+                => access.TryGetValue(k, out var canEdit) && (canEdit || !wantsEdit);
+        }
+    }
 }
