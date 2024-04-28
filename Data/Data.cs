0d30c595d66fb33e4cea69df6213320bcb132489
pmpwsk <flo@uwap.org>
2024-04-28 13:30:32 +0200

    Added GetOrCreateProfile.

--- Data/Data.cs
+++ Data/Data.cs
@@ -5,4 +5,16 @@ namespace uwap.WebFramework.Plugins;
 public partial class FilePlugin : Plugin
 {
     private readonly Table<Profile> Table = Table<Profile>.Import("FilePlugin");
+
+    private Profile GetOrCreateProfile(IRequest req)
+    {
+        string key = $"{req.UserTable.Name}_{req.User.Id}";
+        if (Table.TryGetValue(key, out var profile))
+            return profile;
+
+        Directory.CreateDirectory("../FilePlugin/" + key);
+        profile = new();
+        Table[key] = profile;
+        return profile;
+    }
 }
