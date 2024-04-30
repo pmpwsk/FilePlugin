490fefa780c39974855fae0a3a8cc9f18ce9bbd5
pmpwsk <flo@uwap.org>
2024-04-30 11:09:19 +0200

    Changed the API handler to be "neat".

--- Handlers/API.cs
+++ Handlers/API.cs
@@ -2,28 +2,22 @@ namespace uwap.WebFramework.Plugins;
 
 public partial class FilePlugin : Plugin
 {
-    public override async Task Handle(ApiRequest req, string path, string pathPrefix)
+    protected override async Task<int> HandleNeatly(ApiRequest req, string path, string pathPrefix)
     {
         switch (path)
         {
             case "/add":
             {
                 if (!(req.Query.TryGetValue("u", out var u) && req.Query.TryGetValue("p", out var p) && req.Query.TryGetValue("n", out var n) && n != "" && req.Query.TryGetValue<bool>("d", out var d)))
-                {
-                    req.Status = 400;
-                    break;
-                }
-
+                    return 400;
                 var segments = p.Split('/');
                 CheckAccess(req, u, segments, true, out var profile, out var parent, out var directory, out var file, out var name);
                 if (directory == null)
-                    req.Status = 404;
-                else if (directory.Directories.ContainsKey(n) || directory.Files.ContainsKey(n))
-                    req.Status = 302;
-                else if (profile == null)
-                    req.Status = 500;
-                else
-                {
+                    return 404;
+                if (directory.Directories.ContainsKey(n) || directory.Files.ContainsKey(n))
+                    return 302;
+                if (profile == null)
+                    return 500;
                     profile.Lock();
                     string target = $"../FilePlugin/{req.UserTable.Name}_{u}{string.Join('/', ((IEnumerable<string>)[..segments, n]).Select(Parsers.ToBase64PathSafe))}";
                     if (d)
@@ -37,35 +31,28 @@ public partial class FilePlugin : Plugin
                         directory.Files.Add(n, new(DateTime.UtcNow));
                     }
                     profile.UnlockSave();
-                }
-                
             } break;
 
             case "/load":
             {
                 if (!(req.Query.TryGetValue("u", out var u) && req.Query.TryGetValue("p", out var p)))
-                {
-                    req.Status = 400;
-                    break;
-                }
-                
+                    return 400;
                 var segments = p.Split('/');
                 CheckAccess(req, u, segments, true, out _, out var parent, out var directory, out var file, out var name);
                 if (directory != null)
-                    req.Status = 400;
-                else if (file != null)
-                {
+                    return 400;
+                if (file == null)
+                    return 404;
                     var content = File.ReadAllText($"../FilePlugin/{req.UserTable.Name}_{u}{string.Join('/', segments.Select(Parsers.ToBase64PathSafe))}");
                     if (content == "")
-                        req.Status = 201;
-                    else await req.Write(content);
-                }
-                else req.Status = 404;
+                    return 201;
+                await req.Write(content);
             } break;
 
             default:
-                req.Status = 404;
-                break;
+                return 404;
         }
+
+        return 0;
     }
 }
