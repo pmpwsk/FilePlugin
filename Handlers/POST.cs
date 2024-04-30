206f55f2c58d58a8171b9fc90163c1b41ce19b2c
pmpwsk <flo@uwap.org>
2024-04-30 11:04:01 +0200

    Changed the POST handler to be "neat".

--- Handlers/POST.cs
+++ Handlers/POST.cs
@@ -2,37 +2,34 @@ namespace uwap.WebFramework.Plugins;
 
 public partial class FilePlugin : Plugin
 {
-    public override async Task Handle(PostRequest req, string path, string pathPrefix)
+    protected override async Task<int> HandleNeatly(PostRequest req, string path, string pathPrefix)
     {
         switch (path)
         {
             case "/save":
             {
                 if (!(req.Query.TryGetValue("u", out var u) && req.Query.TryGetValue("p", out var p)))
-                {
-                    req.Status = 400;
-                    break;
-                }
-                
+                    return 400;                
+                if (req.IsForm)
+                    return 400;
                 var segments = p.Split('/');
                 CheckAccess(req, u, segments, true, out var profile, out var parent, out var directory, out var file, out var name);
                 if (directory != null)
-                    req.Status = 400;
-                else if (profile == null)
-                    req.Status = 500;
-                else if (file != null)
-                {
+                    return 400;
+                if (profile == null)
+                    return 500;
+                if (file == null)
+                    return 404;
                     profile.Lock();
                     File.WriteAllText($"../FilePlugin/{req.UserTable.Name}_{u}{string.Join('/', segments.Select(Parsers.ToBase64PathSafe))}", await req.GetBodyText());
                     file.ModifiedUtc = DateTime.UtcNow;
                     profile.UnlockSave();
-                }
-                else req.Status = 404;
             } break;
 
             default:
-                req.Status = 404;
-                break;
+                return 404;
         }
+
+        return 0;
     }
 }
