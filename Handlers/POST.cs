32403b2ab52b4dde2740d8c83aaa9feb237d0ef9
pmpwsk <flo@uwap.org>
2024-04-30 11:07:10 +0200

    Added /upload.

--- Handlers/POST.cs
+++ Handlers/POST.cs
@@ -29,6 +29,52 @@ public partial class FilePlugin : Plugin
                 profile.UnlockSave();
             } break;
 
+            case "/upload":
+            {
+                if (!(req.Query.TryGetValue("u", out var u) && req.Query.TryGetValue("p", out var p)))
+                    return 400;                
+                var segments = p.Split('/');
+                CheckAccess(req, u, segments, true, out var profile, out var parent, out var directory, out var file, out var name);
+                if (directory == null)
+                    return 404;
+                if (profile == null)
+                    return 500;
+                long limit = req.IsAdmin() ? long.MaxValue : 25000000;
+                req.BodySizeLimit = limit;
+                if ((!req.IsForm) || req.Files.Count == 0)
+                    req.Status = 400;
+                profile.Lock();
+                foreach (var uploadedFile in req.Files)
+                {
+                    string loc = $"../FilePlugin/{req.UserTable.Name}_{u}{string.Join('/', ((IEnumerable<string>)[..segments, uploadedFile.FileName]).Select(Parsers.ToBase64PathSafe))}";
+                    long oldSize;
+                    if (directory.Files.TryGetValue(uploadedFile.FileName, out var f))
+                    {
+                        f.ModifiedUtc = DateTime.UtcNow;
+                        oldSize = new FileInfo(loc).Length;
+                    }
+                    else
+                    {
+                        directory.Files[uploadedFile.FileName] = new(DateTime.UtcNow);
+                        oldSize = 0;
+                    }
+                    try
+                    {
+                        uploadedFile.Download(loc, limit);
+                        profile.SizeUsed += new FileInfo(loc).Length - oldSize;
+                    }
+                    catch
+                    {
+                        try { File.Delete(loc); } catch { }
+                        profile.SizeUsed -= oldSize;
+                        directory.Files.Remove(uploadedFile.FileName);
+                        profile.UnlockSave();
+                        throw;
+                    }
+                }
+                profile.UnlockSave();
+            } break;
+
             default:
                 return 404;
         }
