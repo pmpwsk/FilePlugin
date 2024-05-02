314ff0703224ad10bb99fb2832ebde64028f3291
pmpwsk <flo@uwap.org>
2024-05-03 01:52:57 +0200

    Added a download handler to allow the downloading of files.

---
+++ Handlers/Download.cs
@@ -0,0 +1,30 @@
+namespace uwap.WebFramework.Plugins;
+
+public partial class FilePlugin : Plugin
+{
+    protected override async Task<int> HandleNeatly(DownloadRequest req, string path, string pathPrefix)
+    {
+        switch (path)
+        {
+            case "":
+            {
+                if (!(req.Query.TryGetValue("u", out var u) && req.Query.TryGetValue("p", out var p)))
+                    return 400;
+                var segments = p.Split('/');
+                CheckAccess(req, u, segments, true, out var profile, out var parent, out var directory, out var file, out var name);
+                if (directory != null)
+                    return 400;
+                if (profile == null)
+                    return 500;
+                if (file == null)
+                    return 404;
+                await req.SendFile($"../FilePlugin/{req.UserTable.Name}_{u}{string.Join('/', segments.Select(Parsers.ToBase64PathSafe))}", name);
+            } break;
+
+            default:
+                return 404;
+        }
+
+        return 0;
+    }
+}
